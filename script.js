9ad117b49c436c7b82fe59317ab01f037b9b1113
FilipTrajkovski1 <filip13ski@gmail.com>
2026-03-02 18:47:10 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -16,3 +16,12 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const hamburger = document.getElementById("hamburger");
+    const navLinks = document.getElementById("navLinks");
+
+    hamburger.addEventListener("click", function () {
+        navLinks.classList.toggle("active");
+    });
+});
